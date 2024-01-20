9d5783eb72d1931a8c41d64c8d951ff8b398f290
Andres Rubio <a.rubio@4i.ai>
2024-01-20 19:33:49 +0100

    get_fingercode collapsed. added get_contact.

--- hand_detector_desktop.py
+++ hand_detector_desktop.py
@@ -2,8 +2,8 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-fingers = {0:"Pulgar", 1:"Indice", 2:"Corazon",3:"Anular",4:"Menique"}
-
+fingers_dict = {0:"Pulgar", 1:"Indice", 2:"Corazon",3:"Anular",4:"Menique"}
+debug = 0
 
 mp_hands = mp.solutions.hands.Hands()
 mp_hand_connections = mp.solutions.hands_connections.HAND_CONNECTIONS
@@ -12,38 +12,70 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 vid = cv2.VideoCapture(0)
 
-def get_fingercode(landmarks : list):
-    """Takes list of hand_landmarks and returns tuple showing which fingers are extended (joints roughly align) and which dont
+def get_fingercode(landmarks : list, threshold_thumb = 25, threshold = 50):
+    """Takes list of hand_landmarks and returns tuple showing which fingers are extended (joints roughly align) in the first detected hand and which dont
     Tuple order is (p,i,c,a,m), 1 if extended, 0 otherwise
     Landmarks is a results.multi_hand_landmarks list"""
-    fingercode = []
+    fingercode = [0,0,0,0,0]
     if landmarks is not None:
         for detected_hand in landmarks:
             for finger in range(5):
-                art1 = np.array([detected_hand.landmark[1+finger*4].x, detected_hand.landmark[1+finger*4].y,detected_hand.landmark[1+finger*4].z], np.float16)
-                art2 = np.array([detected_hand.landmark[2+finger*4].x, detected_hand.landmark[2+finger*4].y,detected_hand.landmark[2+finger*4].z], np.float16)
-                art3 = np.array([detected_hand.landmark[3+finger*4].x, detected_hand.landmark[3+finger*4].y,detected_hand.landmark[3+finger*4].z], np.float16)
-                art4 = np.array([detected_hand.landmark[4+finger*4].x, detected_hand.landmark[4+finger*4].y,detected_hand.landmark[4+finger*4].z], np.float16)
-                print("Points")
-                print(art1)
-                print(art2)
-                print(art3)
-                print(art4)
-                print("Vectors")
-                print(art2-art1)
-                print(art3-art2)
-                print(art4-art3)
-                vec1 = (art2 - art1) / np.linalg.norm(art2 - art1)
-                vec2 = (art3 - art2) / np.linalg.norm(art3 - art2)
-                vec3 = (art4 - art3) / np.linalg.norm(art4 - art3)
-                print("Normed vectors")
-                print(vec1)
-                print(vec2)
-                print(vec3)
-                ang1 = np.rad2deg(np.arccos(np.clip(np.dot(vec1, vec2), -1.0, 1.0)))
-                ang2 = np.rad2deg(np.arccos(np.clip(np.dot(vec2, vec3), -1.0, 1.0)))
-                print("Finger "+fingers[finger]+" has angles: "+str(ang1)+" "+str(ang2))
-                
+                joints = [None,None,None,None]
+                for joint in range(4):
+                    joints[joint] = np.array([detected_hand.landmark[joint+1+finger*4].x, detected_hand.landmark[joint+1+finger*4].y,detected_hand.landmark[joint+1+finger*4].z], np.float16)
+                if debug:
+                    print("Points")
+                    for joint in joints:
+                        print(joint)
+                    print("Vectors")
+                    for i in range(3):
+                        print(joints[i+1]-joints[i])
+                vectors = [None, None, None]
+                for i in range(3):
+                    vectors[i] = (joints[i+1] - joints[i]) / np.linalg.norm(joints[i+1] - joints[i])
+                if debug:
+                    print("Normed vectors")
+                    for vector in vectors:
+                        print(vector)
+                angles = [None, None]
+                angles[0] = np.rad2deg(np.arccos(np.clip(np.dot(vectors[0], vectors[1]), -1.0, 1.0)))
+                angles[1] = np.rad2deg(np.arccos(np.clip(np.dot(vectors[1], vectors[2]), -1.0, 1.0)))
+                if debug:
+                    print("Finger "+fingers_dict[finger]+" has angles: "+str(angles))
+                if finger == 0:
+                    min_angle = threshold_thumb
+                else:
+                    min_angle = threshold
+                if ((angles[0]+angles[1])/2 < min_angle):
+                    fingercode[finger] = 1
+        return fingercode
+    return None
+
+def get_contact(landmarks : list, threshold = 0.1):
+    """Takes list of hand_landmarks and returns tuple of touching fingertips
+        We assume the thumb is always going to be one of the fingers involved
+        If your anatomy lets you touch multiple fingertips comfortably on one hand go to a chiropractor ig
+        Issue: Threshold is not normalized
+    """
+    if landmarks is not None:
+        for detected_hand in landmarks:
+            fingertips = [None,None,None,None,None]
+            for finger_index in range(5):
+                fingertips[finger_index] = np.array([detected_hand.landmark[4*finger_index+4].x,detected_hand.landmark[4*finger_index+4].y,detected_hand.landmark[4*finger_index+4].z])
+            if debug:
+                print("Fingertips")
+                print(fingertips)
+            distances = [None,None,None,None]
+            for i in range(4):
+                distances[i] = np.linalg.norm(fingertips[i+1]-fingertips[0])
+            if debug:
+                print("Distances")
+                print(distances)
+                print("Closest: "+fingers_dict[np.argmin(distances)+1])
+            if np.amin(distances) < threshold:
+                return np.argmin(distances)+1
+            else:
+                return None
 
 while(True): 
       
@@ -56,7 +88,6 @@ while(True):
     frame.flags.writeable = True
     
     if results.multi_hand_landmarks:
-        get_fingercode(results.multi_hand_landmarks)
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
                 frame,
@@ -66,8 +97,9 @@ while(True):
                 mp_drawing_styles.get_default_hand_connections_style())
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Hands',cv2.flip(frame, 1))
-    get_fingercode(results.multi_hand_landmarks)
-      
+    fingercode = get_fingercode(results.multi_hand_landmarks)  
+    contacts = get_contact(results.multi_hand_landmarks)
+    print(str(fingercode) + " - " + str(contacts))  
     # the 'q' button is set as the 
     # quitting button you may use any 
     # desired button of your choice 

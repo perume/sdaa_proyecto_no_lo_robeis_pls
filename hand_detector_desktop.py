d54afabba43e4cc5226e3e7fd52ca8d71f4c1cba
Andres Rubio <a.rubio@4i.ai>
2024-01-21 14:41:08 +0100

    get_angle & get_direction methods some debugging changes added letter_descriptor.py added CharacterDescriptor & GestureClassifier classes.

--- hand_detector_desktop.py
+++ hand_detector_desktop.py
@@ -1,15 +1,16 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import letter_descriptor as ld
 
-fingers_dict = {0:"Pulgar", 1:"Indice", 2:"Corazon",3:"Anular",4:"Menique"}
-debug = 0
+debug = 1
+show_video = True
 
 mp_hands = mp.solutions.hands.Hands()
 mp_hand_connections = mp.solutions.hands_connections.HAND_CONNECTIONS
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-
+gc = ld.GestureClassifier()
 vid = cv2.VideoCapture(0)
 
 def get_fingercode(landmarks : list, threshold_thumb = 25, threshold = 50):
@@ -23,7 +24,7 @@ def get_fingercode(landmarks : list, threshold_thumb = 25, threshold = 50):
                 joints = [None,None,None,None]
                 for joint in range(4):
                     joints[joint] = np.array([detected_hand.landmark[joint+1+finger*4].x, detected_hand.landmark[joint+1+finger*4].y,detected_hand.landmark[joint+1+finger*4].z], np.float16)
-                if debug:
+                if debug>1:
                     print("Points")
                     for joint in joints:
                         print(joint)
@@ -33,15 +34,15 @@ def get_fingercode(landmarks : list, threshold_thumb = 25, threshold = 50):
                 vectors = [None, None, None]
                 for i in range(3):
                     vectors[i] = (joints[i+1] - joints[i]) / np.linalg.norm(joints[i+1] - joints[i])
-                if debug:
+                if debug>1:
                     print("Normed vectors")
                     for vector in vectors:
                         print(vector)
                 angles = [None, None]
                 angles[0] = np.rad2deg(np.arccos(np.clip(np.dot(vectors[0], vectors[1]), -1.0, 1.0)))
                 angles[1] = np.rad2deg(np.arccos(np.clip(np.dot(vectors[1], vectors[2]), -1.0, 1.0)))
-                if debug:
-                    print("Finger "+fingers_dict[finger]+" has angles: "+str(angles))
+                if debug>1:
+                    print("Finger "+ld.fingers_dict[finger]+" has angles: "+str(angles))
                 if finger == 0:
                     min_angle = threshold_thumb
                 else:
@@ -62,21 +63,67 @@ def get_contact(landmarks : list, threshold = 0.1):
             fingertips = [None,None,None,None,None]
             for finger_index in range(5):
                 fingertips[finger_index] = np.array([detected_hand.landmark[4*finger_index+4].x,detected_hand.landmark[4*finger_index+4].y,detected_hand.landmark[4*finger_index+4].z])
-            if debug:
+            if debug>1:
                 print("Fingertips")
                 print(fingertips)
             distances = [None,None,None,None]
             for i in range(4):
                 distances[i] = np.linalg.norm(fingertips[i+1]-fingertips[0])
-            if debug:
+            if debug>1:
                 print("Distances")
                 print(distances)
-                print("Closest: "+fingers_dict[np.argmin(distances)+1])
+                print("Closest: "+ld.fingers_dict[np.argmin(distances)+1])
             if np.amin(distances) < threshold:
                 return np.argmin(distances)+1
             else:
                 return None
+            
+def get_angle(landmarks : list):
+    """Estimates hand angle by measuring the angle between the wrist coordinates and the middle finger's coordinates
+    Returns:
+    ~ 0º Facing left
+    ~ 90º Facing down
+    ~ -90º Facing up
+    ~ |180º| Facing right
+    """
+    if landmarks is not None:
+        for detected_hand in landmarks:
+            wrist_base = np.array([detected_hand.landmark[0].x,detected_hand.landmark[0].y,detected_hand.landmark[0].z])
+            m_finger_base = np.array([detected_hand.landmark[9].x,detected_hand.landmark[9].y,detected_hand.landmark[9].z])
+            
+            palm_vector = m_finger_base - wrist_base
+            if debug>1:
+                print("Palm angle:")
+                print(np.rad2deg(np.arctan2(palm_vector[1],palm_vector[0])))
+            return np.rad2deg(np.arctan2(palm_vector[1],palm_vector[0]))
+            
+def get_direction(landmarks : list, half_sector_threshold = 30):
+    """Returns the direction the hand is facing if its angle fits between the threshold of a given sector.
+    Returns:
+    None if no direction is clear
+    0 if left
+    1 if up
+    2 if right
+    3 if down
+    """
+    positive = False
+    hand_angle = get_angle(landmarks)
+    if hand_angle is not None:
+        if hand_angle > 0:
+            positive = True
 
+        hand_angle = np.absolute(hand_angle)
+        if hand_angle < 0 + half_sector_threshold:
+            return 0
+        elif hand_angle > 180 - half_sector_threshold:
+            return 2
+        elif hand_angle > 90 - half_sector_threshold and hand_angle < 90 + half_sector_threshold:
+            if positive:
+                return 3
+            else:
+                return 1
+    return None
+    
 while(True): 
       
     # Capture the video frame 
@@ -85,26 +132,30 @@ while(True):
     
     frame.flags.writeable = False
     results = mp_hands.process(frame)
-    frame.flags.writeable = True
     
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                frame,
-                hand_landmarks,
-                mp_hand_connections,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands',cv2.flip(frame, 1))
+    if show_video:
+        frame.flags.writeable = True
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    mp_hand_connections,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Hands',cv2.flip(frame, 1))
+
     fingercode = get_fingercode(results.multi_hand_landmarks)  
     contacts = get_contact(results.multi_hand_landmarks)
-    print(str(fingercode) + " - " + str(contacts))  
-    # the 'q' button is set as the 
-    # quitting button you may use any 
-    # desired button of your choice 
+    direction = get_direction(results.multi_hand_landmarks)
+    
+    if debug:
+        print(str(fingercode) + " - " + str(contacts)+ " - "+ ld.directions_dict[direction])
+        print(gc((fingercode,contacts,direction)))
+
+    # Q - Quit program
     if cv2.waitKey(1) & 0xFF == ord('q'): 
-        #import pdb; pdb.set_trace()
         break
   
 # After the loop release the cap object 
---
+++ letter_descriptor.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+fingers_dict = {0:"Pulgar", 1:"Indice", 2:"Corazon",3:"Anular",4:"Menique"}
+directions_dict = {None: "", 0:"Izq", 1:"Ar", 2:"De", 3:"Ab"}
+
+class CharacterDescriptor():
+    """Describes all known characters based on 3 descriptors: ( ExtendedFingers, Contacts, Direction )
+    ExtendedFingers - List of 5 entries, 1 if finger is extended, starting from thumb
+    Contacts - Int from -1, 1 to 4, -1 means no contacts, 1-4 is the finger contacting the thumb
+    Direction - Int from 0 to 4, specifies whether the palm faces Up or Down, Left or Right. Left & Right are treated the same.
+    If None, that descriptor is irrelevant for this Character's detection"""
+    
+    A = ([1,0,0,0,0],None,0) #LSE, puño horizontal con el pulgar extendido
+    B = ([1,1,1,1,1],None,0) #LSE, palma horizontal
+    C = (None,None,None)
+    D = ([1,1,0,1,1],2,None) #LSE, Contacto P-C, dedos restantes extendidos
+    E = ([0,0,0,0,0],4,1) #ASL, puño cerrado vertical, Contacto P-M
+    F = ([1,0,1,1,1],1,1) #ASL, contacto P-I, vertical, dedos restantes extendidos
+    G = ([0,1,0,0,0],None,0) #LSE, ind extendido horizontal(Apuntar)
+    H = ([0,1,1,0,0],None,0) #LSE, ind + cor extendidos horizontales (Pistola)
+    I = ([0,0,0,0,1],None,0) #LSE*, men extendido horizontal
+    J = ([0,0,0,0,1],None,1) #ASL*, men extendido vertical
+    K = ([0,1,0,0,0],None,1) #ASL*, ind extendido vertical (Pregunta)
+    L = ([1,1,0,0,0],None,1) #ASL, ind + pulgar extendidos vertical (Lerdo)
+    M = ([0,1,1,1,0],None,3) #LSE, ind + cor+ an extendidos hacia abajo
+    N = ([0,1,1,0,0],None,3) #LSE, ind + cor extendidos hacia abajo 
+    O = ([1,0,1,1,1],1,0) #LSE, contacto P-I, horizontal, dedos restantes extendidos (OK)
+    P = ([1,1,1,1,0],None,None) #LSE*, men recogido (Baphomet)
+    Q = ([0,1,0,0,1],None,None) #LSE, men + ind extendidos (Cuernos)
+    R = ([1,1,0,1,1],None,None) #LSE, cor recogido (MiddleFingern't)
+    S = ([1,1,0,0,0],1,None) #LSE*/ASL*, contacto P-I, resto recogidos
+    T = (None,None,None)
+    U = ([1,1,1,0,0],None,1) #ASL*, cor + ind + pul extendidos vertical
+    V = ([0,1,1,0,0],None,1) #LSE/ASL, ind + cor extendidos hacia arriba (Victoria)
+    W = ([0,1,1,1,0],None,1) #ASL, ind + cor + an extendidos hacia arriba
+    X = (None,None,None)
+    Y = ([1,0,0,0,1],None,None) #ASL, pul + men extendidos (RocknRoll)
+    Z = ([1,1,0,0,1],None,None) #LSE*/ASL*, pul + ind + men extendidos (Spiderman) 
+    Middle_Finger = ([0,0,1,0,0],None,1)
+    Open_Palm = ([1,1,1,1,1],None,1)
+    
+class GestureClassifier():
+    
+    @staticmethod
+    def list_to_number(finger_list:list):
+        """Interprets list values as a binary number & returns it"""
+        return finger_list[0]*16 + finger_list[1]*8 + finger_list[2]*4 + finger_list[3]*2 + finger_list[4]
+    
+    def create_classifier_dict(self):
+        """Fills the __classifier__ dict using the class instances
+        Using a dict instead of the CharacterDescriptor class would probably be more Pythonic & Efficient but adding, modifying & documenting
+        gestures seems easier & less error-prone like this"""
+        for code, value in vars(CharacterDescriptor).items():
+            if not code.startswith("__") and not value[0] is None:
+                finger_number = self.list_to_number(value[0])
+                if finger_number in self.__classifier__:   
+                    if type(self.__classifier__[finger_number]) == str:
+                        print("Found match between "+self.__classifier__[finger_number]+" and "+code)
+                        stored_value = self.__classifier__[finger_number]
+                        self.__classifier__[finger_number] = {}
+                        self.__classifier__[finger_number][vars(CharacterDescriptor)[stored_value][2]] = stored_value
+                    if value[2] in self.__classifier__[finger_number]:
+                        if type(self.__classifier__[finger_number][value[2]]) == str:
+                            print("Found match between "+self.__classifier__[finger_number][value[2]]+" and "+code)
+                            stored_value = self.__classifier__[finger_number][value[2]]
+                            self.__classifier__[finger_number][value[2]] = {}
+                            self.__classifier__[finger_number][value[2]][vars(CharacterDescriptor)[stored_value][1]] = stored_value
+                        self.__classifier__[finger_number][value[2]][value[1]] = code   
+                    else:
+                        self.__classifier__[finger_number][value[2]] = code   
+                else:
+                    self.__classifier__[finger_number] = code
+        
+    def __init__(self):
+        self.__classifier__ = {}
+        self.create_classifier_dict()
+        print(self.__classifier__)
+        
+    def __call__(self, hand_info):
+        """Compares given hand_info to the __classifier__ dict object. Returns matched character, or None"""
+        if hand_info[0] is not None:
+            try:
+                result = self.__classifier__[self.list_to_number(hand_info[0])]
+                if type(result) == str:
+                    return result
+                result = result[hand_info[2]]
+                if type(result) == str:
+                    return result
+                result = result[hand_info[1]]
+                if type(result) == str:
+                    return result
+            except KeyError:
+                return None
